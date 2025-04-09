ef2fabdfeeb521d85cfd60dfc32787905ba7449a
daks-dev <daks.dev@ya.ru>
2025-04-09 17:25:50 +0300

    +

--- src/components/astro/drawer/store.ts
+++ src/components/astro/drawer/store.ts
@@ -1,5 +1,3 @@
 import { atom } from 'nanostores';
 
-export const $drawer = atom(false);
-
-// $drawer.listen((x) => document.body.classList[x ? 'add' : 'remove']('app-drawer'));
+export const $drawer = atom<boolean>(false);
