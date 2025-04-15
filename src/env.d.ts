e1d7de1b500dbb88e0638032bba556a980eefb06
daks-dev <daks.dev@ya.ru>
2025-04-16 02:34:03 +0300

    +

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../.astro/types" />
-/// <reference path="@daks.dev/astro.sdk" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="@daks.dev/astro.sdk" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
 /// <reference types="vite-plugin-pwa/info" />
