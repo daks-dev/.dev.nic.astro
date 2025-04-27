2111aff286adf4e866561e8840c27343a6d75730
daks-dev <daks.dev@ya.ru>
2025-04-27 14:10:43 +0300

    +

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -3,8 +3,6 @@ export { default as Header } from './header/Header.astro';
 export { default as Navbar } from './navbar/Navbar.astro';
 export { default as Sidebar } from './sidebar/Sidebar.astro';
 export { default as SidebarToggle } from './sidebar/SidebarToggle.astro';
-export { default as ThemeToggle } from './theme-toggle/ThemeToggle.astro';
-export { default as ToTop } from './to-top/ToTop.astro';
 
 export { default as ActivityCard } from './activity/ActivityCard.astro';
 export { default as ContactsData } from './contacts/ContactsData.astro';
